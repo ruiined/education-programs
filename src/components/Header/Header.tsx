575d3c930794ed3bb4479203ee6ad01f45a89cb8
Maria G <ruiined@gmail.com>
2023-03-23 17:23:31 +0000

    style: header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,9 @@ import "./Header.css";
 
 export const Header = () => {
   return (
-    <header>
-      <h1>All programs</h1>
-      <p>
+    <header className="header-container">
+      <h1 className="header-title">All programs</h1>
+      <p className="header-description">
         In-depth and immersive programs focused on the most critical leadership
         skills and behaviors for today's world. We provide on-campus, online and
         blended program options.
